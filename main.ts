dec3a4bea942b04a72d77e4b11c77cbb6750a9ea
Mankifg <83959255+Mankifg@users.noreply.github.com>
2023-01-16 20:11:42 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+function IntToBinary (num: number) {
+    if (num > PowerTo2[PowerTo2.length - 1]) {
+        basic.showString("Too big")
+    } else {
+        repeat = PowerTo2.length - 1
+        i = 0
+        output = []
+        for (let index = 0; index < repeat; index++) {
+            output.push(0)
+        }
+        for (let index = 0; index < repeat; index++) {
+        	
+        }
+    }
+}
+let output: number[] = []
+let i = 0
+let repeat = 0
+let PowerTo2: number[] = []
+PowerTo2 = [
+1,
+2,
+4,
+8,
+16,
+32
+]
+let n = 9
+IntToBinary(n)
+basic.forever(function () {
+	
+})
